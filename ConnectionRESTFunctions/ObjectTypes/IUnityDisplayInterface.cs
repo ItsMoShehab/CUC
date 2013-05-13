b2b45da301ddfd5c49c9593ae370ba479c68c248
jlindborg <jlindborg@e88f861f-8230-4d52-a54f-cdf66b8ec6c5>
2013-05-13 22:38:19 +0000

    added comparer for the IUnityDisplayInterface objects so lists can be arranged in ascending order easily.

--- ConnectionRESTFunctions/ObjectTypes/IUnityDisplayInterface.cs
+++ ConnectionRESTFunctions/ObjectTypes/IUnityDisplayInterface.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Cisco.UnityConnection.RestFunctions
 {
     /// <summary>
@@ -9,5 +11,23 @@ namespace Cisco.UnityConnection.RestFunctions
     {
         string SelectionDisplayString { get; }
         string UniqueIdentifier { get; }
+    }
+
+    /// <summary>
+    /// Comparison class used for sorting lists of objects that impelement the IunityDisplayInterface
+    /// </summary>
+    public class UnityDisplayObjectCompare : IComparer<IUnityDisplayInterface>
+    {
+        /// <summary>
+        /// Compares two object that implement the IUnityDisplayInterfce based on their display string 
+        /// </summary>
+        /// <returns>
+        /// 0 if items are equal (two null values are considered equal), -1 if item 1 is less than item 2 and 1 if item 1
+        /// is greater than item 2
+        /// </returns>
+        public int Compare(IUnityDisplayInterface pObject1, IUnityDisplayInterface pObject2)
+        {
+            return pObject1.SelectionDisplayString.CompareTo(pObject2.SelectionDisplayString);
+        }
     }
 }
