7f84214b6de8cf1363034ee444e9e4e39921e572
jlindborg <jlindborg@e88f861f-8230-4d52-a54f-cdf66b8ec6c5>
2013-09-03 21:59:04 +0000

    changed logic in RestTransportFunctions around expiring tokens

--- trunk/CUPIVerySimple/Program.cs
+++ trunk/CUPIVerySimple/Program.cs
@@ -38,7 +38,7 @@ namespace CUPIVerySimple
             //attach to server - insert your Connection server name/IP address and login information here.
             try
             {
-                connectionServer = new ConnectionServerRest ("192.168.0.186", "CCMAdministrator", "ecsbulab");
+                connectionServer = new ConnectionServerRest ("lindborgload7.cisco.com", "CCMAdministrator", "ecsbulab");
             }
 
             catch (Exception ex)
